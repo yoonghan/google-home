8e1e4b503fd5a7fa3d3988e414fa6a5b5ade7fe5
han <walcoorperation@gmail.com>
2021-01-04 22:21:22 +0800

    Fix using string format for padding

---
+++ broadcaster-raspberrypi/src/main/java/TriggerClient.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TriggerClient {
+
+	private final int PORT = 9000;
+	private final String HOST_NAME = "10.0.1.1";
+	
+	public TriggerClient() {
+		try (
+		    Socket clientSocket = new Socket(HOST_NAME, PORT);
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		){
+			clientSocket.setKeepAlive(false);
+			clientSocket.setSoTimeout(30_000);
+			
+      out.write("CX:U:"+getMovement(-50,10,0,0));//open arms
+      out.write("AB:L:"+getMovement(360,360,0,0));
+      out.write("CX:U:"+getMovement(50,10,0,0));
+      out.write("AB:L:"+getMovement(-420,420,0,0));
+      out.write("AB:L:"+getMovement(290,290,0,900));//move forward
+      out.write("CX:U:"+getMovement(-50,10,0,0));
+      out.write("AB:L:"+getMovement(-180,-180,0,0));
+
+
+      out.write("AB:L:018001800000000");
+      out.write("CX:U:005000100000000");//close arm
+      out.write("AB:L:0805-8050000000");
+      out.write("AB:L:290029000000900");
+      out.write("AB:L:0440-4400000000");
+      out.write("AB:L:-180-1800000900");
+      out.write("CX:U:-05000100000000");//open arm
+      out.write("AB:L:-360-3600000000");
+
+		    out.flush();
+		    
+		    System.out.println("Completed");
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+
+        private String getMovement(int rotation1, int rotation2, int acceleration, int speed) {
+          return String.format("%04d%04d%04d%03d",rotation1,rotation2,acceleration,speed);
+        }
+
+	public static void main(String args[]) {
+		new TriggerClient();
+	}
+}
--- broadcaster-raspberrypi/src/main/scala/com/walcron/twice/api/LejosCall.scala
+++ broadcaster-raspberrypi/src/main/scala/com/walcron/twice/api/LejosCall.scala
@@ -5,22 +5,44 @@ import java.net._
 
 class LejosCall(ipAddress: String, port: Int) {
   def triggerDevice(shouldRun: Boolean):Unit = {
-    import java.io.PrintWriter
-    import java.net.Socket
+    val inetConnection = new InetSocketAddress(ipAddress, port)
+    val clientSocket = new Socket()
+    clientSocket.connect(inetConnection, 5000)
+    val out = new PrintWriter(clientSocket.getOutputStream, true)
 
     if(shouldRun) {
-      val inetConnection = new InetSocketAddress(ipAddress, port)
-      val clientSocket = new Socket()
-      clientSocket.connect(inetConnection, 5000)
-      val out = new PrintWriter(clientSocket.getOutputStream, true)
-
-      out.write("A:L:03601000100")
-      out.close()
-      clientSocket.close()
+      out.write(createUnregulatedMovement("CX",-50,1))//open
+      out.write(createRegulatedMovement("AB","L",360,360,0,0))
+      out.write(createUnregulatedMovement("CX",50,1))//close
+      out.write(createRegulatedMovement("AB","L",-420,420,0,0))//turn
+      out.write(createRegulatedMovement("AB","L",2900,2900,0,900))
+      out.write(createUnregulatedMovement("CX",-50,1))//close
+      out.write(createRegulatedMovement("AB","L",-180,-180,0,0))
     }
+    else {
+      out.write(createRegulatedMovement("AB","L",180,180,0,0))
+      out.write(createUnregulatedMovement("CX",50,1))//close
+      out.write(createRegulatedMovement("AB","L",805,-805,0,0))
+      out.write(createRegulatedMovement("AB","L",2900,2900,0,900)) 
+      out.write(createRegulatedMovement("AB","L",440,-440,0,0))
+      out.write(createRegulatedMovement("AB","L",-180,-180,0,0))
+      out.write(createUnregulatedMovement("CX",-50,1))
+      out.write(createRegulatedMovement("AB","L",360,360,0,0))
+    }
+
+    out.flush
+    out.close
+    clientSocket.close
+  }
+
+  def createRegulatedMovement(motors:String, motorType:String, rotation1:Int, rotation2:Int, acceleration:Int, speed:Int):String = {
+    String.format("%s:%s:%04d%04d%04d%03d",motors,motorType,rotation1,rotation2,acceleration,speed)
+  }
+
+  def createUnregulatedMovement(motors:String, power:Int, delay:Int):String = {
+    String.format("%s:U:%04d%03d00000000",motors,power,delay)
   }
 
   def stop(): Unit = {
-
   }
 }
